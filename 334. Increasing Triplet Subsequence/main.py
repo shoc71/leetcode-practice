75ac2acb75529f5c82ca841b43a5ebb569c0b83c
Unos <129419169+shoc71@users.noreply.github.com>
2025-04-16 19:42:56 -0400

    Update main.py

--- 334. Increasing Triplet Subsequence/main.py
+++ 334. Increasing Triplet Subsequence/main.py
@@ -1,11 +1,35 @@
 def increasingTriplet(nums: list[int]) -> bool:
+    first_num = float('inf') # unbound interger that can be used to check that are lower value
+    second_num = float('inf')
     
-    pass
+    for num in nums:
+        if num <= first_num:
+            first_num = num
+        elif num <= second_num:
+            second_num = num
+        else:
+            return True
+        
+    return False
 
 test_cases = [
     {"name": "Case 1", "input": [1,2,3,4,5], "expected": True},
     {"name": "Case 2", "input": [5,4,3,2,1], "expected": False},
     {"name": "Case 3", "input": [2,1,5,0,4,6], "expected": True},
+    {"name": "Case 4", "input": [20,100,10,12,5,13], "expected": True},
+    {"name": "Case 5", "input": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                                 1,1,1,1], "expected": False},
+    {"name": "Case 6", "input": [1,2,2147483647], "expected": True},
+    # {"name": "Case 7", "input": [1,2,2147483647], "expected": True},
 ]
 
 for test in test_cases:
